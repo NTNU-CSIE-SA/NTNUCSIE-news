f8bbb523f8dba1d7b848e19eb36f2a5c9b8f03d3
boyan1001 <cby931001@gmail.com>
2025-10-05 00:34:20 +0800

    Test simple discord bot

---
+++ main.py
@@ -0,0 +1,35 @@
+# 導入Discord.py模組
+import discord
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dc_token = os.getenv("DC_TOKEN")
+
+# client是跟discord連接，intents是要求機器人的權限
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
+# 調用event函式庫
+@client.event
+# 當機器人完成啟動
+async def on_ready():
+    print(f"目前登入身份 --> {client.user}")
+
+
+@client.event
+# 當頻道有新訊息
+async def on_message(message):
+    # 排除機器人本身的訊息，避免無限循環
+    if message.author == client.user:
+        return
+    # 新訊息包含Hello，回覆Hello, world!
+    if message.content == "Hello":
+        await message.channel.send("Hello, world!")
+
+
+client.run(dc_token)
